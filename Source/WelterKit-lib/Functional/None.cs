1a649be6ef10c8209a7a28bf4a0b9ed0547b9a99
cyranothedaft <cyranothedev@gmail.com>
2025-03-11 22:47:32 -0400

    initial project; starting to migrate classes from deftware.util

---
+++ Source/WelterKit-lib/Functional/None.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace WelterKit.Functional {
+   public sealed class None : IEquatable<None> {
+      public static None Value { get; } = new None();
+
+      private None() { }
+
+      public override string ToString()
+         => "None";
+
+
+      #region Equality
+
+      public bool Equals(None other) => true;
+      public override int GetHashCode() => 0;
+
+      #endregion Equality
+   }
+}
---
+++ Source/WelterKit-lib/Functional/Unit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace WelterKit.Functional {
+   public sealed class Unit {
+      public static Unit Value = new Unit();
+
+      private Unit() { }
+   }
+}
