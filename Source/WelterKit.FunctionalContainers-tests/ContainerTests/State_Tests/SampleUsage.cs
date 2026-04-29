1a21b55b461dec7be639a65bfd2b83dae0ee5c6a
cyranothedaft <cyranothedev@gmail.com>
2026-04-28 22:30:18 -0400

    State monad wip

---
+++ Source/WelterKit.FunctionalContainers-tests/ContainerTests/State_Tests/SampleUsage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WelterKit.FunctionalContainers_tests.ContainerTests.State_Tests;
+
+[TestClass]
+public class SampleUsage {
+   [TestMethod]
+   public void xxx() {
+
+   }
+
+
+   // gcd :: Int -> Int -> Int
+   // gcd a b | a == b = a
+   //         | a < b  = gcd b a
+   //         | otherwise = gcd b (a - b)
+   //
+   private static int gcd(int a, int b)
+      =>  a == b ? a
+        : a < b  ? gcd(b, a)
+                 : gcd(b, a - b);
+
+
+
+   private record GcdState(int X, int Y);
+
+   //   gcd' :: GCDState -> (Int, GCDState)
+   // gcd' s = 
+   //   let (x, y) = s in   -- unpack the state into x, y
+   //     case compare x y of
+   //       EQ -> (x, s)       -- if x == y, return x and the state tuple
+   //       LT -> gcd' (y, x)  -- if x < y, flip the arguments
+   //       GT -> gcd' (y, x - y)
+   //
+   private static (int, GcdState) gcd_(GcdState s) {
+      (int x,int y) = s;
+      return x.CompareTo(y) switch {
+            0 =>(x,initialState: s),
+            <0=>gcd_(new GcdState( y,x)),
+            _ => gcd_((y,x-y))
+         };
+   }
+}
---
+++ Source/WelterKit.FunctionalContainers/Containers/IState.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using WelterKit.FunctionalContainers.Framework;
+using WelterKit.FunctionalContainers.Framework.Kinds;
+
+
+namespace WelterKit.FunctionalContainers.Containers;
+
+// TODO: abstract or interface
+public interface State<S, A> : K<State, A> {
+
+}
+
+
+public partial class State : IFunctor<State> {
+   public static K<State, B> FMap<A, B>(K<State, A> a, Func<A, B> func)
+      => new State<B>(((State<A>)a).List.Select(func)
+                                   .ToImmutableList());
+}
+
+
+partial class State : IApplicative<State> {
+   public static K<State, A> Pure<A>(A a)
+      => new State<A>([a]);
+
+
+   public static K<State, B> Apply<A, B>(K<State, Func<A, B>> ffunc, K<State, A> fa)
+      => new State<B>((from a in fa.As().List
+                       from func in ffunc.As().List
+                       select func(a)
+                      ).ToImmutableList());
+}
+
+
+
+partial class State : IMonad<State> {
+   // bind :: (M a) -> (a -> M b) -> (M b)
+   // ma >>= f  =  join (fmap f ma)
+   public static K<State, B> Bind<A, B>(K<State, A> ma, Func<A, K<State, B>> f)
+      => join(FMap(ma, f));
+
+
+   // join :: m (m a) -> m a
+   private static K<State, A> join<A>(K<State, K<State, A>> mma)
+      => new State<A>(mma.As().List.SelectMany(innerList => innerList.As().List)
+                                   .ToImmutableList());
+
+}
+
+
+public static class StateExtensions {
+   public static State<A> As<A>(this K<State, A> ma) => (State<A>)ma;
+}
