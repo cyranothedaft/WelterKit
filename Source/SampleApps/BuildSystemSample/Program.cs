7a1eea0b3734fdfc2e838152be85848a49a40922
cyranothedaft <cyranothedev@gmail.com>
2026-05-05 16:55:25 -0400

    wip...

---
+++ Source/SampleApps/BuildSystemSample/Program.cs
@@ -0,0 +1,19 @@
+﻿// using System;
+// using WelterKit.FunctionalContainers.Framework;
+//
+//
+//
+// namespace BuildSystemSample;
+//
+// internal record Task<C, Y, V, F>(Func<Func<Y, K<F, V>>, K<F, V>> taskFunc) where F : K<C, V>;
+//
+// internal class Program {
+//
+//    static Task<C,,K,V> Task()
+//
+//    static void Main(string[] args) {
+//       
+//
+//
+//    }
+// }
