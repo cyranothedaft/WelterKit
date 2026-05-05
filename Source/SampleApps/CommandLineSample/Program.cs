b5d33889e68710bd0bf11ec0f9b2aa502056cb2c
cyranothedaft <cyranothedev@gmail.com>
2026-05-05 13:54:46 -0400

    formatting

--- Source/SampleApps/CommandLineSample/Program.cs
+++ Source/SampleApps/CommandLineSample/Program.cs
@@ -6,22 +6,25 @@ using WelterKit.Extensions.SystemCommandLine;
 using WelterKit.FunctionalContainers.Containers;
 using WelterKit.Std;
 
-
 namespace CommandLineSample;
 
 internal static class Program {
-   static void Main(string[] args)
-      => new RootCommand("Get file system information")
-        .WithSubcommand(new Command("summarize")
-                       .WithArgument(new Argument<DirectoryInfo>("directory") { DefaultValueFactory = _ => new DirectoryInfo(".") },
-                                     out Argument<DirectoryInfo> directoryArgument)
-                       .WithOption(new Option<bool>("--recurse","-r"),
-                                   out  Option<bool> recurseOption)
-                       .WithAction(parse => summarize(parse.GetValue(directoryArgument)!,
-                                                      parse.GetValue(recurseOption)))
-                       )
+   private static void Main(string[] args)
+      => getCommandLineParser()
         .Parse(args)
         .Invoke();
+
+
+   private static RootCommand getCommandLineParser()
+      => new RootCommand("Get file system information")
+           .WithSubcommand(new Command("summarize")
+                          .WithArgument(new Argument<DirectoryInfo>("directory") { DefaultValueFactory = _ => new DirectoryInfo(".") },
+                                        out Argument<DirectoryInfo> directoryArgument)
+                          .WithOption(new Option<bool>("--recurse", "-r"),
+                                      out Option<bool> recurseOption)
+                          .WithAction(parse => summarize(parse.GetValue(directoryArgument)!,
+                                                         parse.GetValue(recurseOption)))
+                          );
 
 
    private static int summarize(DirectoryInfo directory, bool recurse)
@@ -29,9 +32,8 @@ internal static class Program {
 
 
    private static int run(Func<Either<IError, StringBuilder>> command) {
-      return command()
-           .Match(handleResult,
-                  handleError);
+      return command().Match(handleResult,
+                             handleError);
 
       int handleResult(StringBuilder result) {
          Console.WriteLine(result);
--- Source/WelterKit.Std/Errors.cs
+++ Source/WelterKit.Std/Errors.cs
@@ -10,3 +10,15 @@ namespace WelterKit.Std {
       public Exception Exception { get; }
    }
 }
+
+
+
+// TODO: make (something like) this work:
+// public interface IErrorOr<T>;
+// 
+// public abstract record ErrorOr<T> : Either<IError, T>, IErrorOr<T> {
+//    public static implicit operator ErrorOr<T>(IError leftValue ) => new ErrorOr_Left <T>(leftValue);
+//    public static implicit operator ErrorOr<T>(T      rightValue) => new ErrorOr_Right<T>(rightValue);
+// }
+// public          record ErrorOr_Left <T>(IError Error) :Left<IError,T>(Error) ,IErrorOr<T> ;
+// public          record ErrorOr_Right<T>(T Value     ) :Right<IError,T>(Value),IErrorOr<T> ;
