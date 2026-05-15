733ab133aa19b90fefa61fc0f9b7fb0a352cf680
cyranothedaft <cyranothedev@gmail.com>
2026-05-15 10:32:29 -0400

    ...

--- Source/WelterKit.FunctionalContainers/Transformers/ReaderT.cs
+++ Source/WelterKit.FunctionalContainers/Transformers/ReaderT.cs
@@ -13,28 +13,28 @@ public record ReaderT<R, M, A>(Func<R, K<M, A>> RunReaderT) : K<ReaderT<R, M>, A
 
 
 
-public partial class ReaderT<R,M>;
+public partial class ReaderT<R,M> where M : IMonad<M>;
 
 
-partial class ReaderT<R, M> : IFunctor<ReaderT<R, M>> {
+partial class ReaderT<R, M> : IFunctor<ReaderT<R, M>> where M : IMonad<M> {
    public static K<ReaderT<R, M>, B> FMap<A, B>(K<ReaderT<R, M>, A> fa, Func<A, B> func) {
       throw new NotImplementedException();
    }
 }
 
 
-partial class ReaderT<R, M> : IApplicative<ReaderT<R, M>> {
-   public static K<ReaderT<R, M>, A> Pure<A>(A a) {
-      throw new NotImplementedException();
-   }
+partial class ReaderT<R, M> : IApplicative<ReaderT<R, M>> where M : IMonad<M> {
+   // pure a = ReaderT (\_ -> pure a)
+   public static K<ReaderT<R, M>, A> Pure<A>(A a)
+      => new ReaderT<R, M, A>(_ => M.Pure(a));
 
-   public static K<ReaderT<R, M>, B> Apply<A, B>(K<ReaderT<R, M>, Func<A, B>> ffunc, K<ReaderT<R, M>, A> fa) {
-      throw new NotImplementedException();
-   }
+   // (ReaderT f) <*> (ReaderT v) = ReaderT (\r -> f r <*> v r)
+   public static K<ReaderT<R, M>, B> Apply<A, B>(K<ReaderT<R, M>, Func<A, B>> ffunc, K<ReaderT<R, M>, A> fa) 
+   =>
 }
 
 
-partial class ReaderT<R, M> : IMonad<ReaderT<R, M>> {
+partial class ReaderT<R, M> : IMonad<ReaderT<R, M>> where M : IMonad<M> {
    public static K<ReaderT<R, M>, B> Bind<A, B>(K<ReaderT<R, M>, A> ma, Func<A, K<ReaderT<R, M>, B>> f) {
       throw new NotImplementedException();
    }
